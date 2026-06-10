37aa7ef60883616b7343b8a6b9cead9b72572e53
Karl-Markus Hannust <karlmarkushannust@gmail.com>
2026-06-10 23:08:50 +0300

    Better header

--- money-gather-web/src/components/Header.tsx
+++ money-gather-web/src/components/Header.tsx
@@ -1,12 +1,16 @@
 import style from "./Header.module.css"
 
 function Header() {
+
     return(
-      <div className={style.header_div}>
+      <div className={style.header_div} style={{fontFamily: "Simonetta_400Regular"}}>
           <p className={style.logo}>LOGO</p>
-          <a className={style.route}> Home </a>
-          <a className={style.route}> Goals </a>
-          <a className={style.route}> About </a>
+          <a className={style.route}>Home</a>
+          <a className={style.route}>Goals</a>
+          <a className={style.route}>About</a>
+          <a className={style.route + " " + style.right_side}>Log In</a>
+          <a className={style.route}>Sign up </a>
+          <img alt={"profilePic"} src={"profile.png"} className={style.profile_Pic}/>
 
       </div>
     );
